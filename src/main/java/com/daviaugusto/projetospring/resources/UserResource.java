f0a282eb5e53f93b9b575f3857cef20704a63835
Davi Augusto <davi.miranda0919@gmail.com>
2024-12-15 11:42:10 -0300

    Camada de Serviço

--- src/main/java/com/daviaugusto/projetospring/resources/UserResource.java
+++ src/main/java/com/daviaugusto/projetospring/resources/UserResource.java
@@ -1,20 +1,34 @@
 package com.daviaugusto.projetospring.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.daviaugusto.projetospring.entidades.User;
+import com.daviaugusto.projetospring.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 	
+	@Autowired
+	private UserService service;
+	
 	@GetMapping
-	public ResponseEntity<User> buscarTodos(){
-		User u = new User(1L, "Davi", "davi@gmail.com", "9999-9999", "alves0919");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> buscarTodos(){
+		List<User> obj = service.buscarTodos();
+		return ResponseEntity.ok().body(obj);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> buscarPorId(@PathVariable Long id){
+		User obj = service.buscarPorId(id);
+		return ResponseEntity.ok().body(obj);
 	}
 
 }
---
+++ src/main/java/com/daviaugusto/projetospring/services/UserService.java
@@ -0,0 +1,29 @@
+package com.daviaugusto.projetospring.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.daviaugusto.projetospring.entidades.User;
+import com.daviaugusto.projetospring.repositories.UserRepository;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+	
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	
+	public List<User> buscarTodos(){
+		return userRepository.findAll();
+	}
+	
+	public User buscarPorId(Long id) {
+		Optional<User> obj = userRepository.findById(id);
+		return obj.get();
+		
+	}
+
+}
