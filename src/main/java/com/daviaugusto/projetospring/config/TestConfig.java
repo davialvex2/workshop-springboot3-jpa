ddec1e6460857891b6a2a0bb8e105b54cd61c78f
Davi Augusto <davi.miranda0919@gmail.com>
2024-12-26 18:44:54 -0300

    classe payment e suas associações

--- src/main/java/com/daviaugusto/projetospring/config/TestConfig.java
+++ src/main/java/com/daviaugusto/projetospring/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.daviaugusto.projetospring.entidades.Category;
 import com.daviaugusto.projetospring.entidades.Order;
 import com.daviaugusto.projetospring.entidades.OrderItem;
+import com.daviaugusto.projetospring.entidades.Payment;
 import com.daviaugusto.projetospring.entidades.Product;
 import com.daviaugusto.projetospring.entidades.User;
 import com.daviaugusto.projetospring.entidades.enums.OrderStatus;
@@ -86,8 +87,9 @@ public class TestConfig implements CommandLineRunner{
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
 		
-		
-		
+		Payment pay1 = new Payment(null, Instant.parse("2024-06-20T19:53:07Z"), o1);
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
 	}
 	
 }
